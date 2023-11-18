3c3c738e3bab8ad399d3527ea707391028aa5078
Jesper <jesperkallioniemi@gmail.com>
2023-11-18 22:35:34 +0100

    Done
    Done

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import Footer from '../components/Footer/Footer'
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import Buttons from "../components/Generics/Buttons/Buttons";
+
+import "../components/CSS-SCSS/Main/NotFound/NotFound.css";
 
 const NotFound = () => {
   return (
     <>
-        <h1>404 not found</h1>
-        <Footer />
-    </>
-  )
-}
+      <div className="container">
+        <div className="text-area">
+          <h1>404 not found</h1>
 
-export default NotFound
+          <Buttons url="/" type="yellow" title="Return" />
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;
